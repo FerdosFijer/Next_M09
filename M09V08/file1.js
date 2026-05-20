a2918fce5710e1bc2e4cce22248650b846b7281a
FerdosFijer <ffijer@gmail.com>
2026-05-20 19:07:23 +0600

    my second file done

--- M09V08/file1.js
+++ M09V08/file1.js
@@ -1,4 +1,11 @@
-const {a } = require("./file2") 
-const {a : x} = require("./file3") //a:x is not a key payer value, this export and distractureing is called name elias
-
+const {a } = require("./file2");
+const {a : x} = require("./file3"); //a:x is not a key payer value, this export and distractureing is called name elias
 console.log(a, x);
+
+
+const {add} = require("./utlils/add");
+console.log(add(2,4));
+
+const {subs} = require("./utlils/substract")
+console.log(subs(2,6));
+
