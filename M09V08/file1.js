e1bd330406ab7ddd8419d520a80f5797485d7efb
FerdosFijer <ffijer@gmail.com>
2026-05-20 15:08:17 +0600

    Done video

---
+++ M09V08/file1.js
@@ -0,0 +1,4 @@
+const {a } = require("./file2") 
+const {a : x} = require("./file3") //a:x is not a key payer value, this export and distractureing is called name elias
+
+console.log(a, x);
---
+++ M09V08/file2.js
@@ -0,0 +1,6 @@
+const a = 10;
+
+// module.exports = a;
+// console.log(module);
+
+module.exports = {a}; // we will add {} , export value always {} a object and overwrite na howar jonno object ta{} add korbo
